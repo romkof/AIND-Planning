e441bf29db96941ba34c559c7f8cf2c9aaf30752
Roman Fylypets <roman.fylypets@gmail.com>
2017-09-16 20:53:31 +0300

    implemented all methods from AirCargoMethods

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -209,6 +209,9 @@ class AirCargoProblem(Problem):
         """
         # TODO implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig Ed-2 11.2)
         count = 0
+
+        fs = decode_state(node.state, self.state_map).pos
+        count = len(self.goal) - len(list(set(fs).intersection(self.goal)))
         return count
 
 
